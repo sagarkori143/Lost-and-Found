3d8249189118d58d526882fb114e594a554266a6
sagarkori143 <sagarkoriup11@gmail.com>
2024-06-27 23:15:30 +0530

    enhanced login page

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
