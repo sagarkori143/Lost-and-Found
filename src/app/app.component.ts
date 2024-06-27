05e4db6d03ff4754c35d9a21873d73d7135ff685
sagarkori143 <sagarkoriup11@gmail.com>
2024-06-27 23:40:39 +0530

    enhanced navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginPageComponent],
+  imports: [RouterOutlet,LoginPageComponent,HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
